011ce4773a25b387126062a1939bbf63d12051d4
angga1209 <anggayulianto1209@gmail.com>
2023-12-17 17:55:27 +0700

    for & while

---
+++ simple_perulangan_for.py
@@ -0,0 +1,10 @@
+jumlah_buku = 20
+print('Ibu berkata, "Baca semua buku"')
+
+jumlah_buku_terbaca = 0 
+print(f"Jumalah buku yg dibaca {jumlah_buku_terbaca}")
+
+for jumlah_buku_terbaca in range(1,jumlah_buku+1):
+    print(f"Buku {jumlah_buku_terbaca} sudah dibaca")
+
+print(f"Jumlah buku yg sudah dibaca {jumlah_buku_terbaca}")
---
+++ simple_perulangan_while.py
@@ -0,0 +1,12 @@
+jumlah_buku = 10
+print("ibu berkata, baca semua buku")
+
+jumlah_buku_terbaca = 0
+print(f"Jumlah buku yg sudah dubaca {jumlah_buku_terbaca}")
+
+while jumlah_buku_terbaca < jumlah_buku:
+    jumlah_buku_terbaca = jumlah_buku_terbaca + 1
+    print(f"buku ke {jumlah_buku_terbaca} terbaca")
+    
+
+print(f"Jumlah buku yg sudah dibaca {jumlah_buku_terbaca}")
